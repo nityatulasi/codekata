0e460f754846552d5ef6a05b342e1f42ba50b8f8
nityatulasi <52990390+nityatulasi@users.noreply.github.com>
2019-07-17 11:58:29 +0530

    Create char.py

---
+++ char.py
@@ -0,0 +1,5 @@
+ch = input("Please Enter your own charachter");
+if(ord(ch) == 65 or ord(ch) == 69 or ord(ch) == 73 or ord(ch) == 79 or ord(ch) == 85 or ord(ch) == 97 or ord(ch) == 101 or ord(ch) == 105 or ord(ch) == 111 or ord(ch) == 117):
+  print(" Vowel ")
+else:
+  print(" Consonant ")
